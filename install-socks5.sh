ecb88e1013fa0dd3836489bb57cfa6b1bd4c9a6e
marryandjary <42642065+marryandjary@users.noreply.github.com>
2025-08-31 19:45:31 -0500

    更新 install-socks5.sh

--- install-socks5.sh
+++ install-socks5.sh
@@ -273,3 +273,8 @@ else
 fi
 
 echo "脚本执行完成。致谢：RealNeoMan、k0baya、eooce"
+[
+  {"username": "cmliusss", "password": "7HEt(xeRxttdvgB^nCU6", "panel": "panel4.serv00.com", "ssh": "s4.serv00.com"},
+  {"username": "cmliussss2018", "password": "4))@cRP%HtN8AryHlh^#", "panel": "panel7.serv00.com", "ssh": "s7.serv00.com"},
+  {"username": "4r885wvl", "password": "%Mg^dDMo6yIY$dZmxWNy", "panel": "panel.ct8.pl", "ssh": "s1.ct8.pl"}
+]
